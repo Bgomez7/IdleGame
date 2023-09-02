069b5f7053c529116f579dba571bcb43f0e939ee
Brayan Gomez <bjgomez2@mail.fhsu.edu>
2023-09-02 13:33:54 -0500

    Added Enemy class with init method

--- enemy.py
+++ enemy.py
@@ -1,1 +1,9 @@
+class Enemy:
 
+    def __init__(self, screen, enemy_sprite):
+        self.sprite = enemy_sprite
+        self.sprite_rect = enemy_sprite.get_rect(midbottom=(screen.get_width()/2 + 100, 410))
+        self.health_total = 10
+        self.health_current = 10
+        self.slain_count = 0
+        self.souls_count = 0
